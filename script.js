a2ff4996a3744dab1832ce8121fd5eacab424c32
Jao <joao.vfm18@gmail.com>
2021-08-20 20:15:47 -0400

    commit

---
+++ script.js
@@ -0,0 +1,19 @@
+function bt1() {
+    alert("Olá! :D")
+}
+
+function bt2() {
+    alert("Eu gosto de Açai!")
+}
+
+function bt3() {
+    alert("Opa tudo top e ai?")
+}
+
+function bt4() {
+    alert("Vê no Google!!!")
+}
+
+function bt5() {
+    alert("Peixe!")
+}
